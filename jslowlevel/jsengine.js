f90a83482f44a89ea31a1de4b4d52f00a236513b
william <william.del.rosario@iceshop.nl>
2020-03-23 14:38:53 +0100

    hoisting

--- jslowlevel/jsengine.js
+++ jslowlevel/jsengine.js
@@ -14,13 +14,52 @@
 //
 // console.log(jsengine('var a = 5'));
 
-// interpreter vs compiler
-function someCalculation(x, y) {
-    return x + y;
-}
+// // interpreter vs compiler
+// function someCalculation(x, y) {
+//     return x + y;
+// }
+//
+// for (let i = 0; i < 1000; i++) {
+//     someCalculation(5, 4);
+// }
 
-for (let i = 0; i < 1000; i++) {
-    someCalculation(5, 4);
-}
 
+// // hoisting happens on every execution context
+// change to var to const to not rely on hoisting
+// var favouriteFood = "grapes";
+//
+// var foodThoughts = function () {
+//     console.log("Originial fav food: " + favouriteFood);
+//
+//     var favouriteFood = "sushi";
+//
+//     console.log("New fav food: " + favouriteFood)
+// };
+//
+// foodThoughts();
+
+// how hoisting happens
+// 1. creation phase
+var favouriteFood = undefined;
+var foodThoughts = undefined;
+
+// 2. execution phase
+var favouriteFood = "grapes";
+
+// 3. make the function
+foodThoughts = function () {
+    // 7. creation phase
+    var favouriteFood = undefined;
+    console.log("Originial fav food: " + favouriteFood);
+
+    var favouriteFood = "sushi";
+
+    console.log("New fav food: " + favouriteFood)
+};
+
+// 4. run the function
+foodThoughts();
+
+// 5. a new execution context is created
+// 6. inside this execution context, hoisting happens
 
