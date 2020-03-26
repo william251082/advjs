3bd1c9a60f4fc9caeb46e87da0e6b112933a8d62
william <william.del.rosario@iceshop.nl>
2020-03-26 17:11:19 +0100

    problem exercise

--- jslowlevel/node_runtime.js
+++ jslowlevel/node_runtime.js
@@ -5,3 +5,35 @@
 
 console.log(global); // instead of window in the browser
 
+
+// problem
+//fill array with 60000 elements
+// const list = new Array(60000).join('1.1').split('.');
+//
+// function removeItemsFromList() {
+//     var item = list.pop();
+//
+//     if (item) {
+//         removeItemsFromList();
+//     }
+// }
+//
+// removeItemsFromList();
+//
+// console.log(list);
+
+// solution
+//fill array with 60000 elements
+const list = new Array(60000).join('1.1').split('.');
+
+function removeItemsFromList() {
+    var item = list.pop();
+
+    if (item) {
+        setTimeout(removeItemsFromList, 0)
+    }
+}
+
+removeItemsFromList();
+
+console.log(list);
