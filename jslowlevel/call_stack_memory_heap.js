86bb8a221b6de7e96523e8f95ed945120b448bc1
william <william.del.rosario@iceshop.nl>
2020-03-26 15:27:36 +0100

    garbage collection

--- jslowlevel/call_stack_memory_heap.js
+++ jslowlevel/call_stack_memory_heap.js
@@ -1,11 +1,4 @@
 // Call stack + memory heap
-const number = 610;  // allocate memory for number
-const string = 'some text';  // allocate memory for a string
-const human = { // allocate memory for an object... and it's values
-  first: 'John',
-  last: 'Doe'
-};
-
 function subtractTwo(num) {
     return num - 2;
 }
@@ -25,3 +18,16 @@ calculate();
 function inception() {
     inception();
 }
+
+
+// js is a garbage collected language
+// garbage collection
+const number = 610;  // allocate memory for number
+const string = 'some text';  // allocate memory for a string
+var human = { // allocate memory for an object... and it's values
+    first: 'John',
+    last: 'Doe'
+};
+
+// the previous var human has no reference in the memory anymore
+human = 5;
