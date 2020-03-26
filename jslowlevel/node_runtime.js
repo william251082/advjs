6e389bb9586f865085a8e6d7ac2a0f65a36df641
william <william.del.rosario@iceshop.nl>
2020-03-26 17:17:43 +0100

    review

--- jslowlevel/node_runtime.js
+++ jslowlevel/node_runtime.js
@@ -23,17 +23,28 @@ console.log(global); // instead of window in the browser
 // console.log(list);
 
 // solution
-//fill array with 60000 elements
-const list = new Array(60000).join('1.1').split('.');
+// //fill array with 60000 elements
+// const list = new Array(60000).join('1.1').split('.');
+//
+// function removeItemsFromList() {
+//     var item = list.pop();
+//
+//     if (item) {
+//         setTimeout(removeItemsFromList, 0)
+//     }
+// }
+//
+// removeItemsFromList();
+//
+// console.log(list);
 
-function removeItemsFromList() {
-    var item = list.pop();
 
-    if (item) {
-        setTimeout(removeItemsFromList, 0)
-    }
-}
+//1
+setTimeout(()=>{console.log('1', 'is the loneliest number')}, 0);
+setTimeout(()=>{console.log('2', 'can be as bad as one')}, 10);
 
-removeItemsFromList();
+//2
+Promise.resolve('hi').then((data)=> console.log('2', data));
 
-console.log(list);
+//3
+console.log('3','is a crowd');
