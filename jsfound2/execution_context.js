9950c462ab9a45de9f73830303c69cca4865ae4b
william <william.del.rosario@iceshop.nl>
2020-03-26 18:02:24 +0100

    lexical env

--- jsfound2/execution_context.js
+++ jsfound2/execution_context.js
@@ -1,11 +1,24 @@
 // https://coggle.it/diagram/XE3ZoVj-rtA5hcxj/t/advanced-javascript
 
-function printName() { return 'John Doe' }
-function findName() { return printName() }
-function sayMyName() { return findName() }
-
-sayMyName();
+// function printName() { return 'John Doe' }
+// function findName() { return printName() }
+// function sayMyName() { return findName() }
+//
+// sayMyName();
 
 // global === this
 // window === this
 // adding property window.a
+
+// Lexical Environment
+function printName() { return 'John Doe' }
+function findName() {
+    function a() {
+
+    }
+    return printName()
+}
+function sayMyName() { return findName() }
+
+sayMyName();  //window lexical env
+findName.a(); // findName lexical env
