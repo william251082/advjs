627c19debb89d7928e6f5796084945b089f0dde3
william <william.del.rosario@iceshop.nl>
2020-03-26 16:59:44 +0100

    node js --runtime

---
+++ jslowlevel/node_runtime.js
@@ -0,0 +1,7 @@
+// node is server side platform based on async io that is non blocking
+// uses js outside of the browser but creates an entire runtime
+// any async task can be passed to worker threads in the background
+// adds capabilities odf a browser and capabilities outside of the the browser
+
+console.log(global); // instead of window in the browser
+
