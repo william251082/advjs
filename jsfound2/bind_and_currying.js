0ff2edfeb8566c129ded98df075159f29258fbd7
william <william.del.rosario@iceshop.nl>
2020-03-27 18:13:49 +0100

    this keyword

--- jsfound2/bind_and_currying.js
+++ jsfound2/bind_and_currying.js
@@ -4,3 +4,51 @@ console.log(multiply(2, 2));
 
 let multiplyByTwo = multiply.bind(this, 2);
 console.log(multiplyByTwo(2));
+
+
+// this keyword
+var b = {
+    name: 'jay',
+    say() {console.log(this)}
+};
+
+var c = {
+    name: 'jay',
+    say() {return function() {console.log(this)}}
+};
+
+var d = {
+    name: 'jay',
+    say() {return () => console.log(this)}
+};
+
+console.log(b.say());
+console.log(c.say());
+console.log(c.say()());
+console.log(d.say()());
+
+
+//problem
+const character = {
+    name: 'Simon',
+    getCharacter() {
+        return this.name;
+    }
+};
+const giveMeTheCharacterNOW = character.getCharacter;
+
+//How Would you fix this?
+console.log('?', giveMeTheCharacterNOW()); //this should return 'Simon' bud doesn't
+
+
+// solution
+// const character = {
+//     name: 'Simon',
+//     getCharacter() {
+//         return this.name;
+//     }
+// };
+// const giveMeTheCharacterNOW = character.getCharacter.bind(character);
+//
+//
+// console.log('?', giveMeTheCharacterNOW());
