b8b91553ee498efb07e952ca1e9efd8992663428
william <william.del.rosario@iceshop.nl>
2020-03-28 12:16:27 +0100

    weak vs strong typing

--- types/dynvsstattyping.js
+++ types/dynvsstattyping.js
@@ -1,17 +1,17 @@
 // in js type checking is done during runtime
-var a = 100;
-
-// for statically typed lang
-int a;
-a = 100;
+// var a = 100;
+//
+// // for statically typed lang
+// int a;
+// a = 100;
 
 
 //pros of statically typed lang
 // doc for statically typed lang --self-documenting
-function sum(a: number, b: number) {
-    return a + b;
-}
-sum('hello', null);
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
+// sum('hello', null);
 // helps with auto complete
 // less type related bugs on prod
 
@@ -21,4 +21,13 @@ sum('hello', null);
 // slower dev process
 // less time debugging semantics, more debugging time for logic and errors
 
+
+
+// weak typing
+var a = 'hiiiii';
+console.log(a + 17); // hiiiii17
+
+// strong typing
+var a = 'hiiiii';
+console.log(a + 17); // error
 
