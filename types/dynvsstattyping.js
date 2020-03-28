5ad4365d126896d4d1e210fac8729f9b7f943df3
william <william.del.rosario@iceshop.nl>
2020-03-28 12:12:06 +0100

    dynamic vs static typing

---
+++ types/dynvsstattyping.js
@@ -0,0 +1,24 @@
+// in js type checking is done during runtime
+var a = 100;
+
+// for statically typed lang
+int a;
+a = 100;
+
+
+//pros of statically typed lang
+// doc for statically typed lang --self-documenting
+function sum(a: number, b: number) {
+    return a + b;
+}
+sum('hello', null);
+// helps with auto complete
+// less type related bugs on prod
+
+//cons
+// more code, more layer of complexity
+// just write better tests
+// slower dev process
+// less time debugging semantics, more debugging time for logic and errors
+
+
