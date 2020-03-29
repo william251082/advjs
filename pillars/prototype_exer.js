bca82513dfa75835552f9bd9e4caebb90915a0b4
william <william.del.rosario@iceshop.nl>
2020-03-29 15:24:46 +0200

    prototypal inheritance --problem

---
+++ pillars/prototype_exer.js
@@ -0,0 +1,13 @@
+//Exercise - extend the functionality of a built in object
+
+//#1
+//Date object => to have new method .lastYear() which shows you last year 'YYYY' format.
+
+new Date('1900-10-10').lastYear();
+//'1899'
+
+
+//#Bonus
+// Mofify .map() to print 'map_emoji🗺' at the end of each item.
+console.log([1,2,3].map());
+//1🗺map_emoji, 2map_emoji🗺, 3🗺map_emoji
