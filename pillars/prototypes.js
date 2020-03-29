65c803ee967de3598bd040fe8512823ebe52a36e
william <william.del.rosario@iceshop.nl>
2020-03-29 13:51:36 +0200

    prototypal inheritance 1

---
+++ pillars/prototypes.js
@@ -0,0 +1,20 @@
+// // test on the browser console
+// const array = [];
+// // constructor
+// console.log(array.__proto__); // push, pop etc.
+// // base object
+// console.log(array.__proto__.__proto__);  // toString etc
+//
+//
+// function a() {}
+// // constructor
+// console.log(a.__proto__);
+// // base object
+// console.log(a.__proto__.__proto__);
+//
+//
+// const obj1 = {};
+// // constructor // base object
+// console.log(obj1.__proto__);
+
+
