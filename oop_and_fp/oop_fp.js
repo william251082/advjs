0bbbb30c25dc5486dc12d549c08217c6f05f8887
william <william.del.rosario@iceshop.nl>
2020-03-29 20:40:22 +0200

    oop -- constructor Functions

--- oop_and_fp/oop_fp.js
+++ oop_and_fp/oop_fp.js
@@ -29,30 +29,71 @@
 // // problem --methods like attack() should be generic, instead of copying in the memory for every elf
 
 
+// // factory function make/create
+//
+// // Object.create()
+// const elfFunctions = {
+//     attack() {
+//         return'attack with ' + this.weapon
+//     }
+// };
+//
+// function createElf(name, weapon) {
+//     // true prototypal inheritance
+//     let newElf = Object.create(elfFunctions);
+//     console.log(newElf.__proto__);
+//     newElf.name = name;
+//     newElf.weapon = weapon;
+//     return newElf;
+// }
+// const peter = createElf('Peter', 'stones');
+// // peter.attack = elfFunctions.attack;  // instead of creating prop manually, use Object.create()
+// console.log(peter.attack());
+//
+// const sam = createElf('Sam', 'fire');
+// // sam.attack = elfFunctions.attack
+// console.log(sam.attack());
+
+
+
 // factory function make/create
 
-// Object.create()
-const elfFunctions = {
-    attack() {
-        return'attack with ' + this.weapon
-    }
+// constructor functions
+function Elf(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+}
+
+// prototype is useless with loose functions but as constructor function, it's pretty useful because it has call, apply and bind
+Elf.prototype.attack = function () {
+    return 'attack with ' + this.weapon
 };
 
-function createElf(name, weapon) {
-    // true prototypal inheritance
-    let newElf = Object.create(elfFunctions);
-    console.log(newElf.__proto__);
-    newElf.name = name;
-    newElf.weapon = weapon;
-    return newElf;
-}
-const peter = createElf('Peter', 'stones');
-// peter.attack = elfFunctions.attack;  // instead of creating prop manually, use Object.create()
-console.log(peter.attack());
+// lexically scoped arrow function, this id defined to where it is written
+// Elf.prototype.attack = () => {
+//     return 'attack with ' + this.weapon
+// }; // undefined
 
-const sam = createElf('Sam', 'fire');
-// sam.attack = elfFunctions.attack
+// new keyword automatically returns the object and creates the constructor
+// new keyword creates a new execution context --  this is the Object and not window anymore
+const peter = new Elf('Peter', 'stones');
+console.log(peter);
+
+const sam = new Elf('Sam', 'fire');
+console.log(sam.name);
+//sam doesn't have an attak() so js engine will go up the prototype chain and look at the method attack()
 console.log(sam.attack());
+
+
+
+// built-in constructor functions are Object, Function etc. uppercase first letter-best practice
+const Elf1 = new Function('name', 'weapon',
+    `this.name = name;
+    this.weapon = weapon;`);
+
+const sarah = new Elf1('Sarah', 'fireworks');
+console.log(sarah.name);
+
 
 
 
