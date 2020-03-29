07d81633ef5b1f944ec9fceab71cd60e7e91cb8c
william <william.del.rosario@iceshop.nl>
2020-03-29 13:51:50 +0200

    prototypal inheritance 2

--- pillars/prototypes.js
+++ pillars/prototypes.js
@@ -18,3 +18,65 @@
 // console.log(obj1.__proto__);
 
 
+
+// basic
+// let dragon = {
+//     name: 'Tanya',
+//     fire: true,
+//     fight() {
+//         return 5;
+//     },
+//     sing() {
+//         return `I am ${this.name}, the breather of fire.`
+//     }
+// };
+//
+// console.log(dragon.fight());
+// console.log(dragon.sing());
+//
+//
+// let lizard = {
+//   name: 'Kiki',
+//   fight() {
+//       return 1;
+//   }
+// };
+//
+// const singLizard = dragon.sing.bind(lizard);
+// console.log(singLizard());
+
+
+// complex
+let dragon = {
+    name: 'Tanya',
+    fire: true,
+    fight() {
+        return 5;
+    },
+    sing() {
+        if (this.fire) return `I am ${this.name}, the breather of fire.`
+    }
+};
+
+console.log(dragon.fight());
+console.log(dragon.sing());
+
+
+let lizard = {
+    name: 'Kiki',
+    fight() {
+        return 1;
+    }
+};
+
+const singLizard = dragon.sing.bind(lizard);
+// can't inherit fire attr
+console.log(singLizard());
+// inherit everything
+lizard.__proto__ = dragon;
+console.log(dragon);
+console.log(dragon.__proto__); // base object {}
+console.log(dragon.isPrototypeOf(lizard));
+// js will go up the prototype chain to find sing()
+console.log(lizard.sing());
+
