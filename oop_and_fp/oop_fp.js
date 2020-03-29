6551f0c728b4d65c381013fe630b4ca0e3a20ccb
william <william.del.rosario@iceshop.nl>
2020-03-29 19:46:15 +0200

    prototypal inheritance --review

---
+++ oop_and_fp/oop_fp.js
@@ -0,0 +1,29 @@
+// const elf = {
+//     name: name,
+//     weapon: weapon,
+//     attack() {
+//             return 'attack with ' + elf.weapon
+//         }
+// };
+//
+// elf.attack();
+
+
+// factory function make/create
+function createElf(name, weapon) {
+    //we can also have closures here to hide properties from being changed.
+    return {
+        name: name,
+        weapon: weapon,
+        attack() {
+            return 'attack with ' + weapon
+        }
+    }
+}
+const sam = createElf('Sam', 'bow');
+const peter = createElf('Peter', 'bow');
+
+sam.attack();
+peter.attack();
+
+// problem --methods like attack() should be generic, instead of copying in the memory for every elf
