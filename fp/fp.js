ab96e3b8053dfe3aa0a2dd70505c835750556ddf
william <william.del.rosario@iceshop.nl>
2020-03-30 09:36:18 +0200

    fp init

---
+++ fp/fp.js
@@ -0,0 +1,38 @@
+// functional programming --separate data and functions --data and functions are not combined as one object
+// pure functions --separation between data and behaviour of a program
+
+// rpoblme
+// Amazon shopping
+const user = {
+    name: 'Kim',
+    active: true,
+    cart: [],
+    purchases: []
+};
+
+
+//Implement a cart feature:
+// 1. Add items to cart.
+// 2. Add 3% tax to item in cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
+
+//Bonus:
+// accept refunds.
+// Track user history.
+
+
+
+// //Side effects:
+// const array = [1,2,3];
+// function mutateArray(arr) {
+//     arr.pop()
+// }
+// function mutateArray2(arr) {
+//     arr.forEach(item => arr.push(1
+//     ))
+// }
+// //The order of the function calls will matter.
+// mutateArray(array)
+// mutateArray2(array)
+// array
