f6a045b743d9a0c2c702c44fb93e85f1b893fbbf
william <william.del.rosario@iceshop.nl>
2020-04-01 10:32:41 +0200

    composition vs inheritance

--- fp/shopping_cart.js
+++ fp/shopping_cart.js
@@ -91,5 +91,107 @@ console.log(user);
 
 
 // func without a return is just a procedure
-// connect(mapStateToProps, mapDistpatchToProps)(App); // Add data to props
+// connect(mapStateToProps, mapDispatchToProps)(App); // Add data to props
 // mapDispatchToProps and mapStateToProps take actions and returns an object
+
+
+//fp focus more on composition, oop focus more on inheritance
+// Inheritance
+// what it is?
+//
+// class Character needs sleep(), now all the classes that extend Character will need that too
+// tight coupling is the opposite of reusable modular code, coupling of parent and child class is a tight form of coupling
+// tight coupling problem leads to fragile base class problem
+// hierarchy problem, what if the junior class become higher than the boss
+// some low level class will inherit props that it doesn't need
+
+
+// Composition
+// what it has? abilities?
+function attack(character) {
+    return Object.assign({}, character, {attackFn: () => {}} )
+}
+function Elf(name, weapon, type) {
+    let elf = {
+        name, weapon, type
+    }
+}
+
+// Inheritance -- super class the is extended into smaller pieces
+// Composition -- smaller pieces that are combined to make something bigger
+
+// NOT Composition from design pattern book, fragile method
+// Multiple inheritance
+const rectangular = (state) => {
+    return {
+        area: () => {
+            return state.height * state.width;
+        }
+    }
+};
+
+const openable = (state) => {
+    return {
+        toggleOpen: () => {
+            return state.open = !state.open;
+        }
+    }
+};
+
+// to compose them into an object
+const buildRectangularWindow = (state) => {
+  return Object.assign(state, rectangular(state), openable(state));
+};
+
+const rectangularWindow = buildRectangularWindow({
+    height: 20,
+    width: 20,
+    open: false
+});
+
+console.log(rectangularWindow.open);
+console.log(rectangularWindow.toggleOpen());
+
+
+// Composition
+// interface DataReader {
+//     read(): void;
+//     data: string[][];
+// }
+//
+// export class MatchReader {
+//     static fromCsv(filename: string): MatchReader {
+//         return new MatchReader(new CsvFileReader(filename))
+//     }
+//
+//     matches: MatchData[] = [];
+//
+//     constructor(public reader: DataReader) {}
+//
+// load(): void {
+//     this.reader.read();
+// this.matches = this.reader.data.map((row: string[]): MatchData => {
+//     return [
+//         dateStringToDate(row[0]),
+//         row[1],
+//         row[2],
+//         parseInt(row[3]),
+//         parseInt(row[4]),
+//         row[5] as MatchResult,
+//         row[6]
+//
+//     ]
+// });
+// }
+// }
+
+// MatchReader should load up some data and DataReader is only for providing the data
+
+
+// composition                                          inheritance
+// many operations on fixed data                        few operations on common data
+// stateless --not modifying state                      stateful  --you can modify the this inside classes
+// pure functions --no side-effects                     side effects --methods manipulate the internal state
+// declarative --what we want to be doing               imperative --how is it to be done
+// fp is good for processing huge data                  oop is good for characters in a game with few operations
+
