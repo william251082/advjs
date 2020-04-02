5c4c2954c72dce0ab9fe896ead2e3c7778db2b63
william <william.del.rosario@iceshop.nl>
2020-04-02 11:21:37 +0200

    try catch

--- errors/error_handling.js
+++ errors/error_handling.js
@@ -11,11 +11,43 @@
 // myError.stack;
 
 
-function a() {
-    const b = new Error('What??');
-    return b;
-}
-console.log(a());
-console.log(a().stack);
+// function a() {
+//     const b = new Error('What??');
+//     return b;
+// }
+// console.log(a());
+// console.log(a().stack);
 
-// catch the error
+// // catch the error
+// // sunc errors
+// function fail() {
+//     try {
+//         // // this will stop the program
+//         // throw new Error('oopsie!!!!')
+//         // this will go to the catch()
+//         // consoe.log('this works');
+//         console.log('this works');
+//         throw new Error('oopsie!!!!')
+//     } catch (e) {
+//         // handling the error internally
+//         console.log('we have made an oopsie', e)
+//     } finally {
+//         console.log('still good')
+//         return'returning fail';
+//     }
+//     // not gonna be ran
+//     console.log('!!!!!!')
+// }
+// console.log(fail());
+
+// nest try
+try {
+    // async codes don't work
+    try {
+        something();
+    } catch (e) {
+        throw new Error(e)
+    }
+} catch (e) {
+    console.log('got it', e)
+}
